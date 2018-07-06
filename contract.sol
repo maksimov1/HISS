3a119ca5b40e84b9ff269277736c9b0dc638feb5
Sergey <maksimov2135@gmail.com>
2018-07-07 04:35:30 +0700

    v 1
    Version for 1 of 3 step

--- contract.sol
+++ contract.sol
@@ -1,28 +1,82 @@
 pragma solidity ^0.4.21;
 contract Hiss{
-    mapping(address => mapping(uint => string)) hashes; //получаем хеш данных по адреу и номеру записи
-    mapping(address => uint) numberOfNotes; //количество записей для данного адреса
+    address owner;
+    function Hiss(){
+        owner = msg.sender;
+    }
     
-    mapping(string => address) addressByKey; //адрес пациента <=> public Key    
-    mapping(address => string) keyByAddress;
+    mapping(address => mapping(uint => string)) public hashes; //получаем хеш данных по адреу и номеру записи
+    mapping(address => uint) public numberOfNotes; //количество записей для данного адреса
+    
+    mapping(address => bool) public isInsuranceActive; // активна ли страховка для данного пациента
+    
+    //mapping(string => address) public addressByKey; //не надо  
+    mapping(address => string) public keyByAddress;
    
-    mapping(address => mapping(uint => bool)) hospitalAcess; //по адресу пациента смотрим доступна ли запись для
+    mapping(address => bool) public hospitalAcess; //по адресу пациента смотрим доступна ли запись для
                                                              //больницы с этим id (uint)
-    enum typesOfMember {Hospital, Insurance, Patient}
+    mapping(address => string) public hospitalSign;
     
-    mapping(address => typesOfMember) typeOfMember; 
+    
+    enum typesOfMember {NotRegistered, Patient, Hospital, Insurance}
+    
+    mapping(address => typesOfMember) public typeOfMember; 
     
     modifier isHospital(){
         require(typeOfMember[msg.sender] == typesOfMember.Hospital);
         _;
     }
-   
-    function createNote(address adr, string note) public isHospital {
-        hashes[adr][numberOfNotes[adr]] = note;
-        numberOfNotes[adr] = numberOfNotes[adr] + 1;
+    
+    modifier isPatient(){
+        require(typeOfMember[msg.sender] == typesOfMember.Patient);
+        _;
     }
-    //debug function
-    function readNote(address adr, uint numberOfNote) public returns (string){
-        return hashes[adr][numberOfNote];
+    
+    modifier isInsurance(){
+        require(typeOfMember[msg.sender] == typesOfMember.Insurance);
+        _;
+    }
+    
+    modifier isOwner(){
+        require(msg.sender == owner);
+        _;
+    }
+    
+    //мы добавляем страховые
+    function addInsurance (address addr) public isOwner{
+        typeOfMember[addr] = typesOfMember.Insurance;
+    }
+    //страховые добавляют пациентов
+    function addPatient (address addr, string publicKey, string firstNote) public isInsurance{
+        require(typeOfMember[addr] == typesOfMember.NotRegistered);
+        typeOfMember[addr] = typesOfMember.Patient;
+        keyByAddress[addr] = publicKey;
+        hashes[addr][0] = firstNote; // первое посещение
+        numberOfNotes[addr]++;
+        
+    }
+    //страховые добавляют больницы
+    function addHospital (address addr, string digitalSign) public isInsurance{
+        require(typeOfMember[addr] == typesOfMember.NotRegistered);
+        typeOfMember[addr] = typesOfMember.Hospital;
+        hospitalSign[addr] = digitalSign;
+    }
+    //управляют страховкой пациента
+    function setInsurance (address addr, bool flag) public isInsurance{
+        require(typeOfMember[addr] == typesOfMember.Patient);
+        isInsuranceActive[addr] = flag;
+    }
+    
+    //пациент разрешает добавлять новую запись больнице
+    function consentToAddData(address addr) public isPatient{ //сейчас разрешаем на все время потом сделаем(подумаем) на одно
+        require(typeOfMember[addr] == typesOfMember.Hospital);
+        hospitalAcess[addr] = true;
+    }
+    
+    //больница добавляет данные только с разрешения
+    function addNewNote(address addr, string note) public isHospital{
+        require(typeOfMember[addr] == typesOfMember.Patient);
+        hashes[addr][numberOfNotes[addr]] = note;
+        numberOfNotes[addr]++;
     }
 }
