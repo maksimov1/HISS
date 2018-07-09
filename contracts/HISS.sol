5aa1c798f67a90fff88a53b976e002afbd95f7e3
Sergey <maksimov2135@gmail.com>
2018-07-09 11:35:59 +0300

    Change for minmax

--- contracts/HISS.sol
+++ contracts/HISS.sol
@@ -1,14 +1,16 @@
 pragma solidity ^0.4.24;
+
 contract Hiss{
     address public owner;
     uint public total;
     uint public balance;
+    
+    uint public min = 5000000000000000000;
+    uint public max = 10000000000000000000;
+    
     constructor () public {
         owner = msg.sender;
     }
-
-    
-    
     
     mapping(address => mapping(uint => string)) public hashes; //получаем хеш данных по адреу и номеру записи
     mapping(address => uint) public numberOfNotes; //количество записей для данного адреса
@@ -27,74 +29,80 @@ contract Hiss{
     
     mapping(address => typesOfMember) public typeOfMember; 
     
-    modifier isHospital(){
+    modifier isHospital() {
         require(typeOfMember[msg.sender] == typesOfMember.Hospital);
         _;
     }
     
-    modifier isPatient(){
+    modifier isPatient() {
         require(typeOfMember[msg.sender] == typesOfMember.Patient);
         _;
     }
     
-    modifier isInsurance(){
+    modifier isInsurance() {
         require(typeOfMember[msg.sender] == typesOfMember.Insurance);
         _;
     }
     
-    modifier isOwner(){
+    modifier isOwner() {
         require(msg.sender == owner);
         _;
     }
     
-    //мы добавляем страховые
-    function addInsurance (address addr) public isOwner{
+    //владелец сервиса добавляет страховые
+    function addInsurance (address addr) public isOwner {
         typeOfMember[addr] = typesOfMember.Insurance;
     }
     
-    function withdrawal(uint amount) public isOwner{
-        require(amount<=balance);
-        owner.transfer(amount);
-        balance-=amount;
-    }
     
     //страховые добавляют пациентов
-    function addPatient (address addr, string publicKey, string firstNote) public payable isInsurance{
-        require(msg.value >=5000000000000000000 && msg.value<=10000000000000000000);
+    function addPatient (address addr, string publicKey, string firstNote) public payable isInsurance {
+        require(msg.value >= min && msg.value <= max);
         require(typeOfMember[addr] == typesOfMember.NotRegistered);
         typeOfMember[addr] = typesOfMember.Patient;
         keyByAddress[addr] = publicKey;
         hashes[addr][0] = firstNote; // первое посещение
         numberOfNotes[addr]++;
-        balance+=msg.value;
+        balance += msg.value;
     }
     //страховые добавляют больницы
-    function addHospital (address addr, string digitalSign) public payable isInsurance{
-        require(msg.value >=5000000000000000000 && msg.value<=10000000000000000000);
+    function addHospital (address addr, string digitalSign) public payable isInsurance {
+        require(msg.value >= min && msg.value <= max);
         require(typeOfMember[addr] == typesOfMember.NotRegistered);
         typeOfMember[addr] = typesOfMember.Hospital;
         hospitalSign[addr] = digitalSign;
-        balance+=msg.value;
+        balance += msg.value;
     }
-    //управляют страховкой пациента
-    function setInsurance (address addr, bool flag) public isInsurance{
-        
+    //старховые управляют страховкой пациента
+    function setInsurance (address addr, bool flag) public isInsurance {
         require(typeOfMember[addr] == typesOfMember.Patient);
         isInsuranceActive[addr] = flag;
-        
     }
     
     //пациент разрешает добавлять новую запись больнице
-    function consentToAddData(address addr) public isPatient{ //сейчас разрешаем на все время потом сделаем(подумаем) на одно
+    function consentToAddData(address addr) public isPatient { //сейчас разрешаем на все время потом сделаем(подумаем) на одно
         require(typeOfMember[addr] == typesOfMember.Hospital);
         hospitalAccess[addr] = true;
     }
     
     //больница добавляет данные только с разрешения
-    function addNewNote(address addr, string note) public isHospital{
+    function addNewNote(address addr, string note) public isHospital {
         require(typeOfMember[addr] == typesOfMember.Patient);
         require(hospitalAccess[addr] == true);
         hashes[addr][numberOfNotes[addr]] = note;
         numberOfNotes[addr]++;
+    }
+    
+    //вывод средств на кошелек владельца сервиса
+    function withdrawal(uint amount) public isOwner {
+        require(amount <= balance);
+        owner.transfer(amount);
+        balance -= amount;
+    }
+    
+    //изменение минимальной и максимальной стоимостей услуг
+    function setLimits(uint _min, uint _max) public isOwner {
+        min = _min;
+        max = _max;
     }
 }
