9af022b21d3c888a1eec9a923c0ab26d6c2c2ae7
Denis Efremov <efremov@linux.com>
2018-07-12 19:18:05 +0300

    insurance update

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -168,6 +168,100 @@ import HissContractDesc from '../../../build/contracts/Hiss.json';
 						alert(uncrypted);
 					})
 			});
+
+			// Insurance
+			$("#InsuranceButtonAddHospital").click(function () {
+				HissContract.addHospital(
+					$('#InsuranceHAddress').val(),
+					$('#InsuranceHDigitalSign').val(),
+					{ from: account, value: web3.toWei("0.07", 'ether') },
+					(e, myTxHash) => {
+						web3.eth.filter('latest', function (error, result) {
+							web3.eth.getBlock(result, (e, b) => {
+								var t = b.transactions
+								for (var i = 0; i < t.length; i++)
+									if (t[i] == myTxHash) {
+										alert("Медицинское учреждение успешно добавлено");
+									}
+							});
+						});
+					})
+			});
+			$("#InsuranceAddPatient").click(function () {
+				var crypt = new JSEncrypt({ default_key_size: 1024 });
+				crypt.getKey();
+				var PublicKey = crypt.getPublicKey();
+				var PrivateKey = crypt.getPrivateKey();
+				$('#InsurancePPublicKey').val(PublicKey);
+				$('#InsurancePPrivateKey').val(PrivateKey);
+
+				var encryptedFirstNote = crypt.encrypt($('#InsurancePFirstNote').val());
+				console.log(encryptedFirstNote);
+
+				HissContract.addPatient(
+					$('#InsurancePAddress1').val(),
+					PublicKey,
+					encryptedFirstNote,
+					{ from: account, value: web3.toWei("0.07", 'ether') },
+					(e, myTxHash) => {
+						web3.eth.filter('latest', function (error, result) {
+							web3.eth.getBlock(result, (e, b) => {
+								var t = b.transactions
+								for (var i = 0; i < t.length; i++)
+									if (t[i] == myTxHash) {
+										alert("Пациент успешно добавлен");
+									}
+							});
+						});
+					})
+			});
+			$("#InsuranceShowPatientStatus").click(function () {
+				HissContract.isInsuranceActive.call(
+					$('#InsurancePAddress2').val(),
+					(err, res) => {
+						if (err != null) {
+							alert("Произошла ошибка при проверке статуса пациента: " + err);
+						} else {
+							if (res == true) {
+								alert('Страховка действительна');
+							} else {
+								alert('Страховка не действительна');
+							}
+						}
+					})
+			});
+			$("#InsuranceEnablePInsurance").click(function () {
+				HissContract.setInsuranceStatus(
+					$('#InsurancePAddress3').val(),
+					true,
+					(e, myTxHash) => {
+						web3.eth.filter('latest', function (error, result) {
+							web3.eth.getBlock(result, (e, b) => {
+								var t = b.transactions
+								for (var i = 0; i < t.length; i++)
+									if (t[i] == myTxHash) {
+										alert("Страховка активирована");
+									}
+							});
+						});
+					})
+			});
+			$("#InsuranceDisablePInsurance").click(function () {
+				HissContract.setInsuranceStatus(
+					$('#InsurancePAddress3').val(),
+					false,
+					(e, myTxHash) => {
+						web3.eth.filter('latest', function (error, result) {
+							web3.eth.getBlock(result, (e, b) => {
+								var t = b.transactions
+								for (var i = 0; i < t.length; i++)
+									if (t[i] == myTxHash) {
+										alert("Страховка деактивирована");
+									}
+							});
+						});
+					})
+			});
 		}
 		)
 	};
